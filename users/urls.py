fa94ee4d479580639eb3e88cb2f2745649051d58
oguzolgun <oguzolgun44@gmail.com>
2022-06-23 20:20:02 +0300

    flight app created

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
-
+from django.urls import path,include
+from .views import RegisterView
 
 urlpatterns = [
-    ...,
-    path('dj-rest-auth/', include('dj_rest_auth.urls'))
+    
+    path('auth/', include('dj_rest_auth.urls')),
+    path('register/', RegisterView.as_view()),
 ]
